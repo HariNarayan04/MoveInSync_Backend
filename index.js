36e212298f15c3f49094f0059a50b4b4c9a91b20
HariNarayan04 <harinarayan06062004@gmail.com>
2025-11-24 11:02:05 +0530

    Allowed cors for frontend URL

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin :["http://127.0.0.1:5500", "http://localhost:5173"],
+    origin :[process.env.CLIENT_URL, "http://127.0.0.1:5500", "http://localhost:5173"],
     credentials: true
 }));
 
